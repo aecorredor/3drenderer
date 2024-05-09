c88a2544a941642e46a5c38a295084928da9674f
Alejandro Corredor <alejandro.corredor@lifeomic.com>
2024-05-09 17:23:57 -0400

    implement setup func

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <SDL2/SDL.h>
 
 bool is_running = false;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+uint16_t color_buffer = NULL;
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -16,8 +20,8 @@ bool initialize_window(void) {
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        window_width,
+        window_height,
         SDL_WINDOW_BORDERLESS
     );
     if (!window) {
@@ -36,7 +40,7 @@ bool initialize_window(void) {
 }
 
 void setup(void) {
-    // TODO:
+    color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
 }
 
 void process_input(void) {
@@ -67,6 +71,13 @@ void render(void) {
     SDL_RenderPresent(renderer);
 }
 
+void destroy_window(void) {
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
 int main(void) {
     is_running = initialize_window();
 
@@ -78,5 +89,6 @@ int main(void) {
         render();
     }
 
+    destroy_window();
     return 0;
 }
