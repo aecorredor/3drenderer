7033f408e0d34fce30dbdf9718ae00e58274a1b8
Alejandro Corredor <acorredor@stoalabs.com>
2024-05-22 00:02:18 -0400

    add cube rotation

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
-CPPFLAGS := -I/$(shell brew --prefix)/include
-LDFLAGS := -L/$(shell brew --prefix)/lib
+CPPFLAGS := -I$(shell brew --prefix)/include
+LDFLAGS := -L$(shell brew --prefix)/lib
 
 COMPILER = clang
 CFILES = src/*.c
-SOURCE_LIBS = $(CPPFLAGS) $(LDFLAGS) -lSDL2
+SOURCE_LIBS = $(CPPFLAGS) $(LDFLAGS) -lSDL2 -lm
 WARNINGS = -Wall
 BUILD = -g -v -std=c99 -o "binary"
 
--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ const int CUBE_POINT_COUNT = 9 * 9 * 9;
 vec3_t cube_points[CUBE_POINT_COUNT];
 vec2_t projected_points[CUBE_POINT_COUNT];
 vec3_t camera_position = {.x = 0, .y = 0, .z = -5};
+vec3_t cube_rotation = {.x = 0, .y = 0, .z = 0};
 float fov_factor = 640;
 
 void setup(void) {
@@ -40,11 +41,19 @@ vec2_t project(vec3_t point) {
 }
 
 void update(void) {
+  cube_rotation.x += 0.005;
+  cube_rotation.y += 0.005;
+  cube_rotation.z += 0.005;
+
   for (int i = 0; i < CUBE_POINT_COUNT; i++) {
     vec3_t point = cube_points[i];
-    // Move the points away from the camera
-    point.z -= camera_position.z;
-    vec2_t projected_point = project(point);
+    vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
+    transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
+    transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
+
+    // Translate the points away from the camera
+    transformed_point.z -= camera_position.z;
+    vec2_t projected_point = project(transformed_point);
     projected_points[i] = projected_point;
   }
 }
