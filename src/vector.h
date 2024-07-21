ada0507b469426928dfecaf74054367f07a6e1c1
Alejandro Corredor <acorredor@stoalabs.com>
2024-07-21 16:37:19 -0400

    vector add/sub

--- src/vector.h
+++ src/vector.h
@@ -17,12 +17,16 @@ typedef struct {
 //--------------------------------------
 
 float vec2_length(vec2_t v);
+vec2_t vec2_add(vec2_t a, vec2_t b);
+vec2_t vec2_sub(vec2_t a, vec2_t b);
 
 //--------------------------------------
 // 3D
 //--------------------------------------
 
 float vec3_length(vec3_t v);
+vec2_t vec3_add(vec2_t a, vec2_t b);
+vec2_t vec3_sub(vec2_t a, vec2_t b);
 vec3_t vec3_rotate_x(vec3_t v, float angle);
 vec3_t vec3_rotate_y(vec3_t v, float angle);
 vec3_t vec3_rotate_z(vec3_t v, float angle);
